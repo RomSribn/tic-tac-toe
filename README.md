a36f2f51781f1e05b74b7cbdd584ba01844aa72b
RomSribn <steeper.fest@gmail.com>
2019-04-18 23:01:42 +0300

    func handleClick
    handleclicking on cell and drawing X

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # tic-tac-toe
+
+Demo - https://romsribn.github.io/tic-tac-toe/
+
+###### Technologies
+* ES6 
+* fetch API
+* ES6 modules
+* Webpack
+* HTML
+* CSS
+* Git
+#
--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./style.css";
+
+const $gameBord = document.getElementById("game");
+const $gameCells = $gameBord.children;
+
+console.log($gameCells);
+
+for(let i = 0; i < $gameCells.length; i ++){
+    $gameCells[i].addEventListener("click", handleClick)
+}
+
+function handleClick(evt) {
+    const target = evt.target;
+    if(!target.innerHTML){
+        if(target.nodeName === "LI"){
+            target.classList.add("x");
+            target.innerText = "x"
+        }
+    }
+}
