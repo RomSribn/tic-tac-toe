a887b9d1efad3c5021ed0b8abffa39bc376a4c30
RomSribn <steeper.fest@gmail.com>
2019-04-20 17:20:12 +0300

    final 1.0
    basic working functional

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
 import "./style.css";
 import "./spinner.css";
 
+const mainContainer = document.querySelector(".container");
 const $gameBord = document.getElementById("game");
-const $gameCells = $gameBord.children;
+console.log(+$gameBord.children[0].dataset.pos === 0);
 const $spinner = document.querySelector(".spin");
 const startBtn = document.getElementById("reset-game");
 
 const host = "https://ttt-practice.azurewebsites.net/";
 const headers = new Headers();
-headers.append("Content-type", "aplication/json");
+headers.append("Content-type", "application/json");
 
 let id;
-console.log($gameCells);
+let currentPosition;
 
-for(let i = 0; i < $gameCells.length; i ++){
-    $gameCells[i].addEventListener("click", handleClick)
-}
+$gameBord.addEventListener("click", handleClick);
 startBtn.addEventListener("click", startNewGame);
 
-function initializingGame(name) {
+
+
+function  initializingGame(name){
     return fetch(host + "start" + `?name=${name}`).then(response => {
+        $spinner.hidden = false;
         if(response.ok){
             return response.json();
         }
@@ -27,16 +29,32 @@ function initializingGame(name) {
 }
 
 function startNewGame(evt) {
+    deletingCells($gameBord);
+    generateBoardGame().forEach(el => $gameBord.appendChild(el));
     const target = evt.target;
-    target.hidden = true;
-     initializingGame("vasya").then(json => {
+    this.firstElementChild.hidden = true;
+
+    // console.log();
+      initializingGame("vasya")
+          .then(json => {
          if(json.ok){
              $spinner.hidden = true;
-             target.hidden = false;
+             this.firstElementChild.hidden = false;
              id = json.data.id;
-             console.log(id)
+             console.log(json.data);
+             console.log($gameBord);
+             return json;
          }
      });
+
+}
+
+function makeMove(position) {
+    return fetch(host + "makeMove",{
+        method: "POST",
+        body: JSON.stringify({move:+position, id:id, name:"vasya"}),
+        headers: headers
+    })
 
 }
 
@@ -45,7 +63,72 @@ function handleClick(evt) {
     if(!target.innerHTML){
         if(target.nodeName === "LI"){
             target.classList.add("x");
-            target.innerText = "x"
+            target.innerText = "x";
+            currentPosition = target.dataset.pos;
+            makeMove(currentPosition)
+                .then(response => {
+                    if(!response.ok) return;
+                    console.log(response);
+                    return response.json();
+                })
+                .then(response => {
+                    if(!response.ok) return;
+                    console.log(response);
+                    return response.data;
+
+                })
+                .then(data => {
+                    console.log(data);
+                    return waitMove();
+                })
+                .then(response => {
+                    if(!response.ok) return;
+                    return response.json()
+                })
+                .then(response => {
+                    if(!response.ok) return;
+                    console.log(response);
+                    botsMove(response.data.move)
+                })
+
+
         }
+    }
+}
+/*
+data: {move: 4}
+ok: true
+*/
+function botsMove(position) {
+    const nodeArr = $gameBord.children;
+    for(let i = 0; i < nodeArr.length; i ++){
+        if(+nodeArr[i].dataset.pos === position){
+            nodeArr[i].classList.add("o");
+            nodeArr[i].innerText = "o"
+        }
+    }
+}
+
+function waitMove() {
+    return fetch(host + "waitMove",{
+        method: "POST",
+        body: JSON.stringify({name:"vasya", id:id}),
+        headers: headers
+    })
+}
+
+function generateBoardGame(parent) {
+    const cellArr = [];
+    for(let i = 0; i < 9; i ++){
+        const cell = document.createElement("li");
+        cell.setAttribute("data-pos", i);
+        cellArr.push(cell);
+    }
+    return cellArr;
+}
+
+function deletingCells(parent) {
+    while(parent.firstChild){
+        parent.firstChild.remove();
     }
 }
